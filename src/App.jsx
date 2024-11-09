8dda92f9bbee3aff985182ab7e64f1f2b012989d
ACH90 <chassagne.alexandre@gmail.com>
2024-11-09 18:38:57 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Accueil from "./pages/Accueil";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Accueil />
+    </div>
+  );
+}
+
+export default App;
