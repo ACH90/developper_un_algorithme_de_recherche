7170103da6c5cdc70f40b08dae93722bf491959e
ACH90 <chassagne.alexandre@gmail.com>
2024-12-10 13:16:24 +0100

    Modif Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Accueil from "./pages/Home";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Accueil />
+      <Home />
     </div>
   );
 }
