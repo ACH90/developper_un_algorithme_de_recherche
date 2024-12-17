8ff5aa26b04246678f010a1cbb49293d6a19d2af
ACH90 <chassagne.alexandre@gmail.com>
2024-12-17 14:03:05 +0100

    Deploy test 4

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/developper-un-algorithme-de-recherche/",
+  base: "/Developper_un_algorithme_de_recherche/",
 });
