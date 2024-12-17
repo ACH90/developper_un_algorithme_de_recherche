0ac42a6b674098d34e8127d2b79849400f07fd7a
ACH90 <chassagne.alexandre@gmail.com>
2024-12-17 15:37:35 +0100

    Updates

--- src/components/SearchIconButton/SearchIconButton.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./SearchIconButton.module.css";
-
-const SearchIconButton = () => {
-  return <button className={styles.searchIconButton}></button>;
-};
-
-export default SearchIconButton;
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/developper_un_algorithme_de_recherche/",
-});
