bbda4210689e25c1858cb1a9469f3088c5b2660d
ACH90 <chassagne.alexandre@gmail.com>
2024-12-18 12:41:04 +0100

    Test chemin absolu img logo2

--- src/components/Logo/logo.jsx
+++ src/components/Logo/logo.jsx
@@ -3,7 +3,7 @@ import styles from "./logo.module.css";
 const Logo = () => {
   return (
     <div>
-      <img src="public/assets/Logo.png" alt="Logo" className={styles.logo} />
+      <img src="assets/Logo.png" alt="Logo" className={styles.logo} />
     </div>
   );
 };
