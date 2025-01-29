2aa9dbcbfb7cad6d19ff5f3bbe942c3a3d7e4356
ACH90 <chassagne.alexandre@gmail.com>
2025-01-29 15:04:40 +0100

    Dev handeChange SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,12 @@
 /* eslint-disable react/prop-types */
 
 import styles from "./SearchBar.module.css";
+import { useState } from "react";
 import { handleChange, handleClear } from "./SearchBar_Utils";
 
 const SearchBar = ({ inputValue, setInputValue }) => {
+  const [inputQuery, setInputQuery] = useState("");
+
   return (
     <>
       <div className={styles.searchBar}>
@@ -15,15 +18,17 @@ const SearchBar = ({ inputValue, setInputValue }) => {
             className={styles.searchInput}
             placeholder="Rechercher une recette, un ingrédient..."
             aria-label="Search"
-            value={inputValue} // Lier l'état à la valeur de l'input
-            onChange={(event) => handleChange(event, setInputValue)} // Utiliser la fonction handleChange pour chaque changement de l'input
+            value={inputQuery} // Lier l'état à la valeur de l'input
+            onChange={(event) =>
+              handleChange(event, setInputQuery, setInputValue)
+            } // Utiliser la fonction handleChange pour chaque changement de l'input
           />
           <div className={styles.clearIconContainer}>
             {/* Afficher la croix pour effacer le champ si du texte est entré */}
             {inputValue && (
               <span
                 className={styles.clearIcon}
-                onClick={() => handleClear(setInputValue)}
+                onClick={() => handleClear(setInputQuery, setInputValue)}
               >
                 &#x2715; {/* Croix */}
               </span>
--- src/components/SearchBar/SearchBar_Utils.jsx
+++ src/components/SearchBar/SearchBar_Utils.jsx
@@ -1,10 +1,15 @@
 // Fonction de gestion du changement dans l'input
-export const handleChange = (event, setInputValue) => {
+export const handleChange = (event, setInputQuery, setInputValue) => {
   let value = event.target.value;
-  setInputValue(value);
+  setInputQuery(value);
+
+  if (value.length > 2) {
+    setInputValue(value);
+  }
 };
 
 // Fonction pour effacer le texte de l'input
-export const handleClear = (setInputValue) => {
+export const handleClear = (setInputQuery, setInputValue) => {
+  setInputQuery("");
   setInputValue("");
 };
